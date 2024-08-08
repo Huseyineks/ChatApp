77c486eac5786acfbac4fd71a761afecd73b0e7d
Huseyin Eksici <huseyineksici01@gmail.com>
2024-08-08 23:45:24 +0300

    Add project files.

---
+++ ChatApp.DataAccesLayer/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace ChatApp.DataAccesLayer.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+
+        
+    }
+}
